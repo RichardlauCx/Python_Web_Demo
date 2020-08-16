94dff02645ee70f39459342a34940e9a56c85cfc
RichardLau <2409608770@qq.com>
2020-08-16 23:21:23 +0800

    Successful completion of the 1.5 version of storage migration.

---
+++ Django/Study_Notes - V1.5/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\computer\computer_python\python_web\django\study_notes - v1.5\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
